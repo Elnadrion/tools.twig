7e23aac222ccdd0bd5d8188c099f061a6ea9ad71
Elnadrion <elnadrion.dev@gmail.com>
2024-05-08 22:56:22 +0200

    Изменения хелпера склонений

--- src/Elnadrion/Tools/Twig/Extensions/PluralFormExtension.php
+++ src/Elnadrion/Tools/Twig/Extensions/PluralFormExtension.php
@@ -28,22 +28,11 @@ class PluralFormExtension extends TwigAbstractExtension implements TwigGlobalsIn
     /**
      * Выводит правильный вариант множественной формы числа
      *
-     * @param int|float $count Число, для которого нужно сформировать множественную форму (число будет приведено к целому)
-     * @param string[] $input Массив, содержащий 3 слова ['билетов', 'билет', 'билета'] (Ноль билетов, Один билет, Два билета)
+     * @param int $n Число, для которого нужно сформировать множественную форму
+     * @param string[] $forms Массив, содержащий 3 слова ['билетов', 'билет', 'билета'] (Ноль билетов, Один билет, Два билета)
      */
-    public static function russianPluralForm(int|float $count, array $input): string
+    public static function russianPluralForm(int $n, array $forms): string
     {
-        $count = (int)$count;
-        $l2 = substr($count, -2);
-        $l1 = substr($count, -1);
-        if ($l2 > 10 && $l2 < 20) {
-            return $input[0];
-        } else {
-            return match ($l1) {
-                1 => $input[1],
-                2, 3, 4 => $input[2],
-                default => $input[0],
-            };
-        }
+        return $n % 10 == 1 && $n % 100 != 11 ? $forms[0] : ($n % 10 >= 2 && $n % 10 <= 4 && ($n % 100 < 10 || $n % 100 >= 20) ? $forms[1] : $forms[2]);
     }
 }
